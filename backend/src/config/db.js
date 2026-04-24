f1de5bda757c27c8a397ab80715a67741d58a470
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2026-04-24 09:50:56 +0500

    pushed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,8 @@
+import dns from "dns";
 import mongoose from "mongoose";
+
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
